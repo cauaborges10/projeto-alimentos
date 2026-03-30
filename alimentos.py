b1a6a8a1fb2fa35e0b58e7c1a766377fe5303896
cauaborges10 <caucancap@gmail.com>
2026-03-30 11:11:12 -0300

    Refactor food management functions with database integration

--- alimentos.py
+++ alimentos.py
@@ -1,46 +1,87 @@
-from alimentos import inserir_alimento, conectar
+import mysql.connector
 
 
-def limpar():
+# CONEXÃO COM O BANCO
+
+def conectar():
+    return mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="root",
+        database="alimentos"
+    )
+
+
+# GERAR SEC POR GRUPO
+
+def gerar_sec(grupo_id):
+
     conn = conectar()
     cursor = conn.cursor()
-    cursor.execute("DELETE FROM alimentos")
-    conn.commit()
-    conn.close()
 
+    cursor.execute("""
+        SELECT MAX(sec)
+        FROM alimentos
+        WHERE grupo_id = %s
+    """, (grupo_id,))
 
-def test_integracao_simples():
-
-    limpar()
-
-    inserir_alimento("A", "C", "BR")
-
-    conn = conectar()
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT cod, sec FROM alimentos")
     resultado = cursor.fetchone()
 
-    assert resultado[0] == "BRA0001C"
-    assert resultado[1] == 1
+    if resultado[0] is None:
+        sec = 1
+    else:
+        sec = int(resultado[0]) + 1
 
     conn.close()
+    return sec
 
 
-def test_incremento():
+# GERAR CÓDIGO
 
-    limpar()
+def gerar_codigo(pais, grupo_id, tipo_alimento, sec):
 
-    inserir_alimento("A", "C", "BR")
-    inserir_alimento("A", "B", "BR")
+    return f"{pais}{grupo_id}{sec:04d}{tipo_alimento}"
+
+
+# INSERIR NO BANCO
+
+def inserir_alimento(grupo_id, tipo_alimento, pais):
 
     conn = conectar()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT cod FROM alimentos ORDER BY id")
-    resultados = cursor.fetchall()
+    sec = gerar_sec(grupo_id)
+    codigo = gerar_codigo(pais, grupo_id, tipo_alimento, sec)
 
-    assert resultados[0][0] == "BRA0001C"
-    assert resultados[1][0] == "BRA0002B"
+    sql = """
+    INSERT INTO alimentos (cod, sec, grupo_id, tipo_alimento, pais)
+    VALUES (%s, %s, %s, %s, %s)
+    """
+
+    cursor.execute(sql, (codigo, sec, grupo_id, tipo_alimento, pais))
+    conn.commit()
 
     conn.close()
+
+    print("\nAlimento inserido com sucesso!")
+    print("SEC:", sec)
+    print("Código:", codigo)
+
+    return codigo
+
+
+# PROGRAMA PRINCIPAL
+
+def main():
+
+    print("Cadastro de Alimentos\n")
+
+    grupo_id = input("Digite o grupo (A, B, C...): ").upper()
+    tipo_alimento = input("Digite o tipo de alimento: ").upper()
+    pais = input("Digite o país (ex: BR): ").upper()
+
+    inserir_alimento(grupo_id, tipo_alimento, pais)
+
+
+if __name__ == "__main__":
+    main()
